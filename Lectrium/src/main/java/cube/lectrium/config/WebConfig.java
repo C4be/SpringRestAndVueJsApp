0da6d52bf55068f8f354a8b847d23c9d62fa6f05
SuperCube <dmitrycube@yandex.ru>
2025-02-22 23:05:49 +0300

    fix

--- Lectrium/src/main/java/cube/lectrium/config/WebConfig.java
+++ Lectrium/src/main/java/cube/lectrium/config/WebConfig.java
@@ -1,5 +1,6 @@
 package cube.lectrium.config;
 
+import org.springframework.lang.NonNull;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:8082")  // Разрешаем доступ с фронтенда
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Все нужные методы
