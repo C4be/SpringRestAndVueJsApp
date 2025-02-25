244519416fb463e3af55ca5b0b71de3c59062398
C4be <dmitryc4be@gmail.com>
2025-02-25 14:57:27 +0300

    add swagger-doc

--- Lectrium/src/main/java/cube/lectrium/model/mapper/CellMapper.java
+++ Lectrium/src/main/java/cube/lectrium/model/mapper/CellMapper.java
@@ -1,11 +1,17 @@
 package cube.lectrium.model.mapper;
 
 import cube.lectrium.model.Cell;
+import cube.lectrium.model.NoteBook;
 import cube.lectrium.model.dto.CellDTO;
 import org.springframework.stereotype.Component;
+import cube.lectrium.repository.NoteBookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Component
 public class CellMapper {
+
+    @Autowired
+    private NoteBookRepository notebookRepository;  // Репозиторий для получения Notebook
 
     public CellDTO toDTO(Cell cell) {
         return CellDTO.builder()
@@ -13,15 +19,21 @@ public class CellMapper {
                 .content(cell.getContent())
                 .cellType(cell.getCellType())
                 .numberInSequence(cell.getNumberInSequence())
+                .notebookId(cell.getNotebook().getId())  // связывание с ноутбуком
                 .build();
     }
 
     public Cell toEntity(CellDTO dto) {
+        // Получаем объект Notebook из базы данных по ID
+        NoteBook notebook = notebookRepository.findById(dto.getNotebookId())
+                .orElseThrow(() -> new IllegalArgumentException("Notebook not found"));
+
         return Cell.builder()
                 .title(dto.getTitle())
                 .content(dto.getContent())
                 .cellType(dto.getCellType())
                 .numberInSequence(dto.getNumberInSequence())
+                .notebook(notebook)  // связывание с объектом ноутбука
                 .build();
     }
 }
